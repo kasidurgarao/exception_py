b1c552ba1618dbb836d5fb53476d76d33d320d15
kasi Durgarao <kasidurgarao183@gmail.com>
2024-10-28 11:16:10 +0530

    new_lin_add

--- div.py
+++ div.py
@@ -3,6 +3,7 @@ try:
     input2 = int(input("enter the 2nd value"))
     result = input1/input2
     print(result)
+#new line is addedd
 except ZeroDivisionError:
     print("enter the other than 0 in input2")
 
